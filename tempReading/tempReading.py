b81fe8f6c236d31a3be79a6a0b23b437fd637fd0
Hayato Tsujii <97589878+CaptianHa@users.noreply.github.com>
2024-02-07 00:13:00 -0800

    adding comments

--- tempReading/tempReading.py
+++ tempReading/tempReading.py
@@ -9,24 +9,26 @@ image = cv2.imread(image_path, cv2.IMREAD_ANYDEPTH)
 # Check if the image is read successfully
 if image is not None:
    
-   # Display the temperature value at the specified (x, y) coordinates
+    # Display the temperature value at the specified (x, y) coordinates
     x = 165
     y = 115
     pixelFrame = image[y,x]
- 
+    # Convert pixel values to temperature in Celsius, idk if the values are correct
     pixelFrame = (pixelFrame / 100) -273.15
-
+    #adds color to the photo
     image = cv2.applyColorMap(image,cv2.COLORMAP_INFERNO)
-
+    #adds a small circle in where the temperature is measured
     cv2.circle(image,(x,y), 2, (0,0,0), -1)
-    # Convert pixel values to temperature in Celsius
-    #temperature = (image - offset) * scale
+    
+    #adds the temperature reading near the circle
     cv2.putText(image,"{0:.1f} Celius".format(pixelFrame), (x-80, y-15),cv2.FONT_HERSHEY_PLAIN,1,(255,0,0),2)
  
-    #print(f"Temperature at ({x}, {y}): {pixelFrame[x, y]:.2f} Celsius")
+    #prints the the temp val
     print(pixelFrame)
     # Display the thermal image
     cv2.imshow('Temperature Image', image)
+    #exits if there is any key pressed
     cv2.waitKey(0)
+#unsuccessful
 else:
     print("Error reading the image.")
