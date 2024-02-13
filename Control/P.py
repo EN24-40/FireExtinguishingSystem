5a9a28ca6e7748959a866511bb13d3690fc6b94e
kingof14 <157544342+kingof14@users.noreply.github.com>
2024-02-13 14:30:20 -0800

    P.py

--- Control/P.py
+++ Control/P.py
@@ -9,14 +9,24 @@ Kp = 0.8
 Ts = 0.01
 SP = 5
 
+lower = SP - (0.03*SP)
+upper = SP + (0.03*SP)
 
-# Sample every Ts seconds:
-if (time == Ts):
-    time = time - Ts
-#    act_meas = readADC()
+on = 1
 
-    e = SP - act_meas
+while on:
+    # Sample every Ts seconds:
+    if (time == Ts):
+        time = time - Ts
+#        act_meas = readADC()
 
-    u = e * Kp
+        e = SP - act_meas
 
-#    writeDA(u)
+        u = e * Kp
+
+#        writeDA(u)
+        
+        # Exit if within threshold
+        if (u>lower) & (u<upper):
+            on = 0
+            
