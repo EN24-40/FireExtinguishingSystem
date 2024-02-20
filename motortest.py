1a6d161fd88b8c4e83c25202505f3ff7a7fec4be
EOsgood1 <etosgood@gmail.com>
2024-02-20 18:44:18 +0000

    some pwm changes

--- motortest.py
+++ motortest.py
@@ -1,37 +1,45 @@
-from gpiozero import PWMLED
-import keyboard
+import RPi.GPIO as GPIO
 import time
 
-# Set up the GPIO pins
-pin_w = PWMLED(20)  # for 'w' key
-pin_s = PWMLED(21)  # for 's' key
+pin_extend = 21  # for 'w' key
+pin_retract = 20  # for 's' key
+
+GPIO.setwarnings(False)			#disable warnings
+GPIO.setmode(GPIO.BCM)		#set pin numbering system
+GPIO.setup(pin_extend, GPIO.OUT)
+pi_pwm_extend = GPIO.PWM(pin_extend, 100)		#create PWM instance with frequency
+pi_pwm_extend.start(0)	
+
+GPIO.setup(pin_retract, GPIO.OUT)
+pi_pwm_retract = GPIO.PWM(pin_retract, 100)		#create PWM instance with frequency
+pi_pwm_retract.start(0)	
 
 try:
     while True:
         
-        pin_w.value = 1
+        print("extending")
+        pi_pwm_extend.ChangeDutyCycle(50)
+        time.sleep(2)
+        pi_pwm_extend.ChangeDutyCycle(0)
         time.sleep(1)
-        pin_w.value = 0
-        time.sleep(0.5)
-        pin_s.value = 1
+        print("retracting")
+        pi_pwm_retract.ChangeDutyCycle(50)
+        time.sleep(2)
+        pi_pwm_retract.ChangeDutyCycle(0)
         time.sleep(1)
-        pin_s.value = 0
-        time.sleep(0.5)
 
         # for i in range(50):
-        #     pin_w.value = 1
+        #     pin_extend.value = 1
         #     time.sleep(0.1)
 
-        # pin_w.off()
+        # pin_extend.off()
 
         # for i in range(50):
-        #     pin_s.value = 1
+        #     pin_retract.value = 1
         #     time.sleep(0.1)
 
-        # pin_s.off()
+        # pin_retract.off()
 
 except KeyboardInterrupt:
     # Clean up GPIO state
-    pin_w.off()
-    pin_s.off()
     print("\nScript terminated by user.")
