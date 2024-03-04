0488b7e5b669bc95d509c2d5638784c952f4e2c9
EOsgood1 <etosgood@gmail.com>
2024-03-04 04:07:36 +0000

    update

--- homography.py
+++ homography.py
@@ -7,18 +7,18 @@ import os
 
 # Points in source (real world coordinates)
 # Bret under hoop, back corner of house, Connor near garage, Connor bottom right corner
-pts_src = np.array([[0, -15.666,1], [40, -15.666,1], [23, 5,1], [15, 0, 1]]) # in FT (N+, S-, E+, W-)
+pts_src = np.array([[-15.666, 1,1], [-15.666, 40,1], [5, 23,1], [1, 15, 1]]) # in FT (N+, S-, E+, W-)
 
 # Points in camera image (pixels)
-pts_dst = np.array([[34, 175, 1], [101, 88, 1], [174, 109,1], [300, 180, 1]])  # in Pixels
+pts_dst = np.array([[-126, 65,1], [-59, 154, 1], [14, 131, 1], [140, 60, 1]])  # in Pixels
 
 # Calculate H in homography equation
 h, status = cv2.findHomography(pts_src, pts_dst)
 
 inv_h = np.linalg.inv(h)
 
-spot = np.array([[140],
-                [86], 
+spot = np.array([[0],
+                [154], 
                 [1]])
 
 prod = np.dot(inv_h, spot)
