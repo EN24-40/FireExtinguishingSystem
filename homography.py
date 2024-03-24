6f46d4ec28b976d8f272b13d1a182ee727751320
EOsgood1 <etosgood@gmail.com>
2024-03-24 20:35:59 +0000

    loop for data testing

--- homography.py
+++ homography.py
@@ -1,34 +1,94 @@
 import numpy as np
 import cv2
-import os
+#importing the hotspot variables 
+# from hotspot import cX
+# from hotspot import cY
 
-
-#image_src = cv2.imread('')
 
 # Points in source (real world coordinates)
 # Bret under hoop, back corner of house, Connor near garage, Connor bottom right corner
-pts_src = np.array([[-15.666, 1,1], [-15.666, 40,1], [5, 23,1], [1, 15, 1]]) # in FT (N+, S-, E+, W-)
+pts_src = np.array([[-15.666, 1,1], [-15.666, 40,1], [5, 23,1], [15, 1, 1]]) # in FT 
 
 # Points in camera image (pixels)
-pts_dst = np.array([[-126, 65,1], [-59, 154, 1], [14, 131, 1], [140, 60, 1]])  # in Pixels
+pts_dst = np.array([[-126, 65,1], [-126, 154, 1], [14, 131, 1], [140, 60, 1]])  # in Pixels
 
 # Calculate H in homography equation
 h, status = cv2.findHomography(pts_src, pts_dst)
 
+#inversing so we can get real world coordinates
 inv_h = np.linalg.inv(h)
 
-spot = np.array([[0],
-                [154], 
-                [1]])
 
-prod = np.dot(inv_h, spot)
 
-val = 1 /prod[2]   
 
-final = np.multiply(val, prod)
+#a loop for testing many spots if wanted.
+multiple_value_testing = True
 
-#print(inv_h.size())
-print(h)
-print('\n')
-print('\n')
-print(final)
+if multiple_value_testing == True:
+
+    # These are the min and max "x" and "y" (IN PIXELS) to get real world coords from
+    x_max_val = 160      #changable max value to iterate through
+    x_min_val = -160
+    x_step = 10    #value to iterate by, sapce between coords
+
+    y_max_val = 260
+    y_min_val = 0
+    y_step = 1    #value to iterate by, sapce between coords
+
+    # Some pretty printing stuff, ignore.
+    print("X Pix\t Y Pix\t X Real (ft)\t\t Y Real (ft)")
+    print("-----------------------------------------------------------------")
+
+    # Double loop, takes an x value, goes through all y values for that x, then goes to next x
+    for x in list(range(x_min_val, x_max_val + 1, x_step)):
+        for y in list(range(y_min_val, y_max_val + 1, y_step)):
+
+            # Pixel location to get real world coords for
+            spot = np.array([[x],
+                             [y],
+                             [1]])
+
+            # Dot product h matrix with pixel matrix
+            prod = np.dot(inv_h, spot)
+
+            # Reciprocal of z value, used to multiply matrix to get answer
+            val = 1 / prod[2]
+
+            # Multiply matrix by reciprocal to make z = 1 and therefore other vals (x, y) are in real world coord (ft)
+            final = np.multiply(val, prod)
+
+            # If Y Pixel is less than 60 (it seems), the y value in feet is negative, which is not possible
+            # so it is actually just the minimum value of y = 1.
+            # if final[1] < 0:
+            #     final[1] = 1
+            
+            # Loop print values: X pixel, Y Pixel, X Real Coord, Y Real Coord
+            print(x, "\t", y, "\t", final[0][0], "\t", final[1][0])
+
+
+
+else:   # In the case of else, a single point can be input and used. Automation uses this.
+
+    #putting the hotspot in an array
+    spot = np.array([[150],
+                    [70], 
+                    [1]])
+
+    prod = np.dot(inv_h, spot)
+
+    #val is the reciprocal of the z axis number
+    val = 1 /prod[2]   
+    #multiplying so z is 1
+    final = np.multiply(val, prod)
+
+    # If real Y value is negative (pixel below 60 input for Y), then Y is actually = 1
+    if final[1] < 0:
+        final[1] = 1
+
+    #print(inv_h.size())
+    print(h)
+    print('\n')
+    print('\n')
+
+
+    print(final[0][0], "\t", final[1][0])
