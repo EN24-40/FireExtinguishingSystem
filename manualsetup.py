a057222655ea1d719ca5667ae96db3c24b1b1cd2
kingof14 <157544342+kingof14@users.noreply.github.com>
2024-03-25 10:32:55 -0700

    Update manualsetup.py

--- manualsetup.py
+++ manualsetup.py
@@ -5,7 +5,13 @@ class MyController(Controller):
 
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
+        
+    def on_x_press(self):
+       print("Hello world")
+
+    def on_x_release(self):
+       print("Goodbye world")
 
 
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
-controller.listen()
+controller.listen(timeout=60)
